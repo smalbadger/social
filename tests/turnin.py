12a45d045b1e446fdeea25cd5f2790475650c4a8
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2020-07-17 01:45:50 -0700

    defeat the pin

--- tests/turnin.py
+++ tests/turnin.py
@@ -7,36 +7,94 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 
+def get_pin():
+    import imaplib
+    import email
+
+    imaplib._MAXLINE = 1000000
+
+    EMAIL = 'social.hackers.468@gmail.com'
+    PASSWORD = 'linkedin.tester468'
+    SERVER = 'imap.gmail.com'
+
+    # connect to the server and go to its inbox
+    mail = imaplib.IMAP4_SSL(SERVER)
+    mail.login(EMAIL, PASSWORD)
+    # we choose the inbox but you can select others
+    mail.select('inbox')
+
+    # we'll search using the ALL criteria to retrieve
+    # every message inside the inbox
+    # it will return with its status and a list of ids
+    status, data = mail.search(None, 'ALL')
+    # the list returned is a list of bytes separated
+    # by white spaces on this format: [b'1 2 3', b'4 5 6']
+    # so, to separate it first we create an empty list
+    mail_ids = []
+    # then we go through the list splitting its blocks
+    # of bytes and appending to the mail_ids list
+    for block in data:
+        # the split function called without parameter
+        # transforms the text or bytes into a list using
+        # as separator the white spaces:
+        # b'1 2 3'.split() => [b'1', b'2', b'3']
+        mail_ids += block.split()
+
+    # now for every id we'll fetch the email
+    # to extract its content
+    for i in mail_ids:
+        # the fetch function fetch the email given its id
+        # and format that you want the message to be
+        status, data = mail.fetch(i, '(RFC822)')
+
+        # the content data at the '(RFC822)' format comes on
+        # a list with a tuple with header, content, and the closing
+        # byte b')'
+        for response_part in data:
+            # so if its a tuple...
+            if isinstance(response_part, tuple):
+                # we go for the content at its second element
+                # skipping the header at the first and the closing
+                # at the third
+                message = email.message_from_bytes(response_part[1])
+
+                # with the content we can extract the info about
+                # who sent the message and its subject
+                mail_from = message['from']
+                mail_subject = message['subject']
+
+                # then for the text we have a little more work to do
+                # because it can be in plain text or multipart
+                # if its not plain text we need to separate the message
+                # from its annexes to get the text
+                if message.is_multipart():
+                    mail_content = ''
+
+                    # on multipart we have the text message and
+                    # another things like annex, and html version
+                    # of the message, in that case we loop through
+                    # the email payload
+                    for part in message.get_payload():
+                        # if the content type is text/plain
+                        # we extract it
+                        if part.get_content_type() == 'text/plain':
+                            mail_content += part.get_payload()
+                else:
+                    # if the message isn't multipart, just extract it
+                    mail_content = message.get_payload()
+
+                # and then let's show its result
+                print(f'From: {mail_from}')
+                print(f'Subject: {mail_subject}')
+                print(f'Content: {mail_content}')
+
+                msg = "Please use this verification code to complete your sign in: "
+                if msg in mail_content:
+                    return mail_content.split(msg)[1][:5]
+    return ""
+
+
 class LoginForm(unittest.TestCase):
-    # def setUp(self):
-    #
-    #     # Put your username and authkey below
-    #     # You can find your authkey at crossbrowsertesting.com/account
-    #     self.username = os.environ.get('CBT_USERNAME')
-    #     self.authkey = os.environ.get('CBT_AUTHKEY')
-    #
-    #     self.api_session = requests.Session()
-    #     self.api_session.auth = (self.username, self.authkey)
-    #
-    #     self.test_result = None
-    #
-    #     caps = {}
-    #
-    #     caps['name'] = 'Github Actions Example'
-    #     caps['browserName'] = 'Chrome'
-    #     caps['platform'] = 'Windows 10'
-    #     caps['screenResolution'] = '1366x768'
-    #     caps['username'] = self.username
-    #     caps['password'] = self.authkey
-    #     caps['record_video'] = 'true'
-    #
-    #     self.driver = webdriver.Remote(
-    #         desired_capabilities=caps,
-    #         # command_executor="http://%s:%s@hub.crossbrowsertesting.com:80/wd/hub"%(self.username,self.authkey)
-    #         command_executor="http://hub.crossbrowsertesting.com:80/wd/hub"
-    #     )
-    #
-    #     self.driver.implicitly_wait(20)
 
     def test_social(self):
 
@@ -76,6 +134,21 @@ class LoginForm(unittest.TestCase):
         time.sleep(1)
 
         try:
+            browser.find_element_by_class_name("profile-background-image")
+        except:
+
+            time.sleep(1)
+            pin_verification = browser.find_element_by_id("input__email_verification_pin")
+
+            pin = ""
+            while not pin:
+                pin = get_pin()
+
+            pin_verification.send_keys(pin)
+            pin_verification.send_keys(Keys.RETURN)
+            time.sleep(1)
+
+        try:
             browser.find_element_by_css_selector(
                 "a[data-control-name=overlay.maximize_connection_list_bar]").click()
         except InvalidSelectorException as e:
@@ -104,15 +177,6 @@ class LoginForm(unittest.TestCase):
         msg_send.click()
 
         browser.close()
-
-    # def tearDown(self):
-    #     print("Done with session %s" % self.driver.session_id)
-    #     self.driver.quit()
-    #     # Here we make the api call to set the test's score.
-    #     # Pass it it passes, fail if an assertion fails, unset if the test didn't finish
-    #     if self.test_result is not None:
-    #         self.api_session.put('https://crossbrowsertesting.com/api/v3/selenium/' + self.driver.session_id,
-    #                              data={'action': 'set_score', 'score': self.test_result})
 
 
 if __name__ == '__main__':
