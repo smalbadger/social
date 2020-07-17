98da3cfd6aacde445a9b6d91ba37cf1f3cdc82cf
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2020-07-17 13:12:16 -0700

    modularized pin retrieval from email

--- tests/turnin.py
+++ tests/turnin.py
@@ -6,92 +6,122 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
+import imaplib
+from datetime import datetime, timedelta
+import email
 
-def get_pin():
-    import imaplib
-    import email
+imaplib._MAXLINE = 1000000
 
-    imaplib._MAXLINE = 1000000
+
+class PinValidation:
 
     EMAIL = 'social.hackers.468@gmail.com'
     PASSWORD = 'linkedin.tester468'
     SERVER = 'imap.gmail.com'
 
-    # connect to the server and go to its inbox
-    mail = imaplib.IMAP4_SSL(SERVER)
-    mail.login(EMAIL, PASSWORD)
-    # we choose the inbox but you can select others
-    mail.select('inbox')
+    def __init__(self):
+        # connect to the server and go to its inbox
+        self.mailbox = imaplib.IMAP4_SSL(PinValidation.SERVER)
+        self.mailbox.login(PinValidation.EMAIL, PinValidation.PASSWORD)
+        # we choose the inbox but you can select others
+        self.mailbox.select('inbox')
 
-    # we'll search using the ALL criteria to retrieve
-    # every message inside the inbox
-    # it will return with its status and a list of ids
-    status, data = mail.search(None, 'ALL')
-    # the list returned is a list of bytes separated
-    # by white spaces on this format: [b'1 2 3', b'4 5 6']
-    # so, to separate it first we create an empty list
-    mail_ids = []
-    # then we go through the list splitting its blocks
-    # of bytes and appending to the mail_ids list
-    for block in data:
-        # the split function called without parameter
-        # transforms the text or bytes into a list using
-        # as separator the white spaces:
-        # b'1 2 3'.split() => [b'1', b'2', b'3']
-        mail_ids += block.split()
+    def get_pin(self, username:str, email_origin:'str', timeout:timedelta, time:datetime = None) -> str:
+        """
+        Searches the inbox for a pin validation message belonging to the specific user.
 
-    # now for every id we'll fetch the email
-    # to extract its content
-    for i in mail_ids:
-        # the fetch function fetch the email given its id
-        # and format that you want the message to be
-        status, data = mail.fetch(i, '(RFC822)')
+        :param username: The full linkedin username of the person to search for
+        :param email_origin: The email belonging to the user for which we're validating the pin.
+        :param timeout: The amount of time we want to try getting the pin before giving up
+        :param time: The approximate time that the pin was sent.
+        :return: The 6-digit validation pin
+        """
 
-        # the content data at the '(RFC822)' format comes on
-        # a list with a tuple with header, content, and the closing
-        # byte b')'
-        for response_part in data:
-            # so if its a tuple...
-            if isinstance(response_part, tuple):
-                # we go for the content at its second element
-                # skipping the header at the first and the closing
-                # at the third
-                message = email.message_from_bytes(response_part[1])
+        # we'll search using the ALL criteria to retrieve every message inside the inbox it will return with its status
+        # and a list of ids
+        needed_msg = "Please use this verification code to complete your sign in: "
+        query = f'(FROM "security-noreply@linkedin.com" TO "{email_origin}" TEXT "{needed_msg}")'
 
-                # with the content we can extract the info about
-                # who sent the message and its subject
-                mail_from = message['from']
-                mail_subject = message['subject']
+        pin = ""
+        start = datetime.now()
+        while not pin:
 
-                # then for the text we have a little more work to do
-                # because it can be in plain text or multipart
-                # if its not plain text we need to separate the message
-                # from its annexes to get the text
-                if message.is_multipart():
-                    mail_content = ''
+            now = datetime.now()
+            time_running = now - start
+            if time_running > timeout:
+                raise Exception("Timeout while searching for the pin validation email")
 
-                    # on multipart we have the text message and
-                    # another things like annex, and html version
-                    # of the message, in that case we loop through
-                    # the email payload
-                    for part in message.get_payload():
-                        # if the content type is text/plain
-                        # we extract it
-                        if part.get_content_type() == 'text/plain':
-                            mail_content += part.get_payload()
-                else:
-                    # if the message isn't multipart, just extract it
-                    mail_content = message.get_payload()
+            status, data = self.mailbox.search(None, query)
 
-                # and then let's show its result
-                # print(f'From: {mail_from}')
-                # print(f'Subject: {mail_subject}')
-                # print(f'Content: {mail_content}')
+            # the list returned is a list of bytes separated by white spaces on this format: [b'1 2 3', b'4 5 6'] so, to
+            # separate it first we create an empty list
+            mail_ids = []
 
-                msg = "Please use this verification code to complete your sign in: "
-                if msg in mail_content:
-                    return mail_content.split(msg)[1][:6]
-    return ""
+            # then we go through the list splitting its blocks of bytes and appending to the mail_ids list
+            for block in data:
+                # the split function called without parameter transforms the text or bytes into a list using as separator
+                # the white spaces:
+                # b'1 2 3'.split() => [b'1', b'2', b'3']
+                mail_ids += block.split()
+
+            # now for every id we'll fetch the email
+            # to extract its content
+
+            for i in mail_ids:
+
+                now = datetime.now()
+                time_running = now - start
+                if time_running > timeout:
+                    raise Exception("Timeout while searching for the pin validation email")
+
+                # the fetch function fetch the email given its id
+                # and format that you want the message to be
+                status, data = self.mailbox.fetch(i, '(RFC822)')
+
+                # the content data at the '(RFC822)' format comes on
+                # a list with a tuple with header, content, and the closing
+                # byte b')'
+                for response_part in data:
+                    # so if its a tuple...
+                    if isinstance(response_part, tuple):
+                        # we go for the content at its second element
+                        # skipping the header at the first and the closing
+                        # at the third
+                        message = email.message_from_bytes(response_part[1])
+
+                        # with the content we can extract the info about
+                        # who sent the message and its subject
+                        mail_from = message['from']
+                        mail_subject = message['subject']
+
+                        # then for the text we have a little more work to do
+                        # because it can be in plain text or multipart
+                        # if its not plain text we need to separate the message
+                        # from its annexes to get the text
+                        if message.is_multipart():
+                            mail_content = ''
+
+                            # on multipart we have the text message and
+                            # another things like annex, and html version
+                            # of the message, in that case we loop through
+                            # the email payload
+                            for part in message.get_payload():
+                                # if the content type is text/plain
+                                # we extract it
+                                if part.get_content_type() == 'text/plain':
+                                    mail_content += part.get_payload()
+                        else:
+                            # if the message isn't multipart, just extract it
+                            mail_content = message.get_payload()
+
+                        # and then let's show its result
+                        # print(f'From: {mail_from}')
+                        # print(f'Subject: {mail_subject}')
+                        # print(f'Content: {mail_content}')
+
+                        if needed_msg in mail_content:
+                            return mail_content.split(needed_msg)[1][:6]
+        return ""
 
 
 class LoginForm(unittest.TestCase):
@@ -134,15 +164,16 @@ class LoginForm(unittest.TestCase):
         time.sleep(1)
 
         try:
-            browser.find_element_by_class_name("profile-background-image")
+            browser.find_element_by_class_name("profile-rail-card__default-bg")
         except:
 
             time.sleep(1)
             pin_verification = browser.find_element_by_id("input__email_verification_pin")
 
-            pin = ""
-            while not pin:
-                pin = get_pin()
+
+            timeout = timedelta(minutes=15)
+            pin = PinValidation().get_pin("ÔỐỒỔỖỘÔỐỒỔỖỘôốồổỗộôố ƯỨỪỬỮỰƯỨỪỬỮỰưứừửữựưứ",
+                                          "linkedin.test11@facade-technologies.com", timeout)
 
             print(pin)
             pin_verification.send_keys(pin)
@@ -178,6 +209,12 @@ class LoginForm(unittest.TestCase):
         msg_send.click()
 
         browser.close()
+
+    def test_email(self):
+        timeout = timedelta(minutes=15)
+        pin = PinValidation().get_pin("ÔỐỒỔỖỘÔỐỒỔỖỘôốồổỗộôố ƯỨỪỬỮỰƯỨỪỬỮỰưứừửữựưứ",
+                                      "linkedin.test11@facade-technologies.com", timeout)
+        assert pin == "859721"
 
 
 if __name__ == '__main__':
