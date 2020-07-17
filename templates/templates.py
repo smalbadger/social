a8c3a5ec251043ec28dc9d243c3b0141b2afc035
Philippe Cutillas <pcutillas@email.arizona.edu>
2020-07-16 19:33:51 -0700

    changed file for redundancy

--- templates/templates.py
+++ templates/templates.py
@@ -1,14 +1,14 @@
-import os
 import pathlib
+from os import listdir
 from os.path import isfile, join
 
 path = pathlib.Path(__file__).parent.absolute()
-files = [f for f in os.listdir(path) if isfile(join(path, f)) and f.endswith('.txt')]
+files = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith('.txt')]
 
 TEMPLATE = {}
 
 for file in files:
-    with open(os.path.join(path, file), 'r') as f:
+    with open(join(path, file), 'r') as f:
         TEMPLATE[int(file[:-4])] = f.read()
 
         # This allows us to later get a template based on familiarity level,
