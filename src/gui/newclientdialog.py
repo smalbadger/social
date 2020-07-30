78291dcaab411157550b3327418395e6698e539d
Sam Badger <smalbadger@smalbadger@email.arizona.edu>
2020-07-30 01:23:38 -0700

    Finished client creation and fixed up the GUI a bit.

--- src/gui/newclientdialog.py
+++ src/gui/newclientdialog.py
@@ -1,8 +1,15 @@
+from PySide2.QtWidgets import QDialog, QMessageBox, QProgressDialog
+from PySide2.QtCore import QThreadPool, Signal
+
 from gui.ui.ui_newclientdialog import Ui_Dialog
-from PySide2.QtWidgets import QDialog, QMessageBox
+
+from database.linkedin import session, Client, LinkedInAccount
+from common.threading import Task
 
 
 class NewClientDialog(QDialog):
+
+    clientCreated = Signal(Client)
 
     def __init__(self):
         QDialog.__init__(self)
@@ -10,31 +17,69 @@ class NewClientDialog(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
+        self.ui.tabWidget.setTabEnabled(1, False) # disable facebook (not implemented)
+        self.ui.tabWidget.setTabEnabled(2, False) # disable instagram (not implemented)
+        self.ui.tabWidget.setTabEnabled(3, False) # disable twitter (not implemented)
+
+        self.ui.errorLabel.hide()
+
     def accept(self):
+
+        self.ui.errorLabel.hide()
+
         clientName = self.ui.clientName.text()
-        noCredentials = True
+        clientEmail = self.ui.clientEmail.text()
+        clientPhone = self.ui.clientPhone.text()
+        isTester = self.ui.testerBox.isChecked()
+        self.client = Client(name=clientName, email=clientEmail, phone=clientPhone, tester=isTester)
+        dbEntries = [self.client]
+
+        accounts = 0
+        errors = []
 
         if not clientName:
-            QMessageBox.warning(self, 'Error', 'Please enter a client name.', QMessageBox.Ok)
+            QMessageBox.critical(self, 'Error', 'Please enter a client name.', QMessageBox.Ok)
             return
 
-        linkedIn = {
-            'username': self.ui.linkedInUsername.text(),
-            'password': self.ui.linkedInPassword.text()
-        }
-        facebook = {
-            'username': self.ui.facebookUsername.text(),
-            'password': self.ui.facebookPassword.text()
-        }
-        insta = {
-            'username': self.ui.instaUsername.text(),
-            'password': self.ui.instaPassword.text()
-        }
-        twitter = {
-            'username': self.ui.twitterUsername.text(),
-            'password': self.ui.twitterPassword.text()
-        }
+        u, e, p = self.ui.linkedInUsername.text(), self.ui.linkedInEmail.text(), self.ui.linkedInPassword.text()
+        if u or e or p:
+            if not u:
+                errors.append("Need LinkedIn Username")
+            elif not e:
+                errors.append("Need LinkedIn Email")
+            #TODO: Force password storage once we have the encryption down
+            # elif not p:
+            #     errors.append("Need LinkedIn Password")
+            else:
+                accounts += 1
+                linkedinAccount = LinkedInAccount(email=e, username=u, password=p, tester=isTester)
+                self.client.linkedin_account = linkedinAccount
+                dbEntries.append(linkedinAccount)
 
-        if linkedIn['username'] and linkedIn['password']:
-            noCredentials = False
+        # TODO: Implement Facebook, Instagram, and LinkedIn as needed.
 
+        if errors:
+            self.ui.errorLabel.setText("\n".join(errors))
+            self.ui.errorLabel.show()
+            return
+
+        if not accounts:
+            response = QMessageBox.question(self, "Warning", "Are you sure you'd like to create a client with no accounts?", QMessageBox.Yes, QMessageBox.No)
+            if response == QMessageBox.No:
+                return
+
+        prog = QProgressDialog("Creating new client. please wait...", "Hide", 0, 0, parent=self)
+        prog.setModal(True)
+
+        def addToDB():
+            session.add_all(dbEntries)
+            session.commit()
+            self.clientCreated.emit(self.client)
+            prog.close()
+
+        task = Task(addToDB)
+        task.finished.connect(prog.close)
+        QThreadPool.globalInstance().start(task)
+        prog.exec_()
+
+        super().accept()
